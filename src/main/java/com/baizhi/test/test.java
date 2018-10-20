f266ed4ce865f1ae6bdb827838f1db4bc3cb41e3
djf <2810878408@qq.com>
2018-10-20 16:00:53 +0800

    this istestgit

--- src/main/java/com/baizhi/test/test.java
+++ src/main/java/com/baizhi/test/test.java
@@ -4,5 +4,8 @@ package com.baizhi.test;
  * Created by Administrator on 2018/10/20.
  */
 public class test {
+    public static void main(String[] args) {
+        System.out.println("你是最棒的");
+    }
 
 }
